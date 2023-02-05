1fd3348ff9c3ab729ddf985301547bc1a9d7e347
Asvel <fe.asvel@gmail.com>
2023-02-06 00:40:13 +0800

    add speaker name

--- scripts/Program.cs
+++ scripts/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text;
+using System.Xml;
 using AssetsTools.NET.Extra;
 using YamlDotNet.Core;
 using YamlDotNet.Serialization.EventEmitters;
@@ -7,10 +8,26 @@ using YamlDotNet.Serialization;
 var dataPath = args[0];
 var langs = new string[] { "jpja", "cnch", "usen" };
 
-var transRaw = new Dictionary<string, List<string>>();
+var personIdToTextId = new Dictionary<string, string>();  // eg. リュール => MPID_Lueur, マルス => MGID_Marth
+foreach (var (file, keyField, valueField) in new[] { ("person", "Pid", "Name"), ("god", "Gid", "Mid") })
+{
+    var am = new AssetsManager();
+    var bun = am.LoadBundleFile(Path.Combine(dataPath, $@"StreamingAssets\aa\Switch\fe_assets_gamedata\{file}.xml.bundle"));
+    var inst = am.LoadAssetsFileFromBundle(bun, 0, true);
+    var inf = inst.table.GetAssetsOfType((int)AssetClassID.TextAsset)[0];
+    var xmlData = am.GetTypeInstance(inst, inf).GetBaseField()["m_Script"].GetValue().AsString();
+    var xml = new XmlDocument();
+    xml.LoadXml(xmlData[1..]);  // BOM
+    foreach (XmlNode node in xml.DocumentElement!.SelectSingleNode("/Book/Sheet")!.SelectNodes("Data/Param")!)
+    {
+        personIdToTextId.TryAdd(node.Attributes![keyField]!.InnerText[4..], node.Attributes[valueField]!.InnerText);
+    }
+}
+
 var texts = new Dictionary<string, Dictionary<string, List<string>>>();
 foreach (var lang in langs)
 {
+    var textIdToText = new Dictionary<string, string>();
     var dir = Path.Combine(dataPath, @"StreamingAssets\aa\Switch\fe_assets_message" , lang[..2], lang);
     foreach (var path in Directory.GetFiles(dir))
     {
@@ -84,6 +101,13 @@ foreach (var lang in langs)
                             var type2 = reader.ReadUInt16();
                             var parameterSize = reader.ReadUInt16();
                             var parameters = reader.ReadBytes(parameterSize);
+                            if (type1 == 3 && type2 == 2)  // speaker
+                            {
+                                if (BitConverter.ToUInt16(parameters) != parameters.Length - 2) throw new InvalidDataException();
+                                txt.Append('\u000e');
+                                txt.Append(personIdToTextId[Encoding.Unicode.GetString(parameters[2..])]);
+                                txt.Append('\u000e');
+                            }
                             if (type1 != 2 && type1 != 3 && type1 != 4 && type1 != 5 &&
                                 type1 != 7 && type1 != 11 && !(type1 == 6 && type2 == 0))
                             {
@@ -121,6 +145,25 @@ foreach (var lang in langs)
         {
             if (lang == langs[0]) texts[filename].Add(lbl, new());
             texts[filename][lbl].Add(txt.Trim());
+
+            if (lbl.StartsWith("MPID_") || lbl.StartsWith("MGID_"))
+            {
+                textIdToText.Add(lbl, txt);
+            }
+        }
+    }
+    foreach (var file in texts.Values)
+    {
+        foreach (var txts in file.Values)
+        {
+            var txt = txts[^1];
+            var index = txt.IndexOf('\u000e');
+            if (index != -1)
+            {
+                var endIndex = txt.IndexOf('\u000e', index + 1);
+                var textId = txt[(index + 1)..(endIndex)];
+                txts[^1] = string.Concat(txt[..index], "<", textIdToText[textId], ">", txt[(endIndex + 1)..]);
+            }
         }
     }
 }
